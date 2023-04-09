6af20988f939527db0e354cb53e143545f127659
Jiexuan <81137599+botcon02@users.noreply.github.com>
2023-04-09 17:49:41 +0800

    Improve default templates and sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -30,19 +30,20 @@ public class SampleDataUtil {
         Item sword = new Item(new Name("Rusty Sword"), 3, 3, new HashSet<>(Arrays.asList(rustyTag)));
         Item bow = new Item(new Name("Rusty Bow"), 4, 3, new HashSet<>(Arrays.asList(rustyTag)));
         Item staff = new Item(new Name("Glorious Staff"), 20, 20, new HashSet<>());
+        Item spellBook = new Item(new Name("Spellbook"), 5, 1, new HashSet<>());
         Inventory i1 = new Inventory(Arrays.asList(new Item[] {sword}));
         Inventory i2 = new Inventory(Arrays.asList(new Item[] {bow}));
-        Inventory i3 = new Inventory(Arrays.asList(new Item[] {staff}));
+        Inventory i3 = new Inventory(Arrays.asList(new Item[] {staff, spellBook}));
         Character c = new Character(new Name("Mike"), new Stats(3, 3, 3), 3, 2, i2, new HashSet<>());
         Mob m1 = new Mob(new Name("Skeleton Archer"), new Stats(20, 20, 20), 2,
                 false, i2, new HashSet<>(Arrays.asList(undeadTag)));
         Mob m2 = new Mob(new Name("Skeleton Warrior"), new Stats(20, 20, 20), 2,
                 false, i1, new HashSet<>(Arrays.asList(undeadTag)));
-        Mob m3 = new Mob(new Name("Skeleton Necromancer"), new Stats(300, 300, 300), 5,
+        Mob m3 = new Mob(new Name("Necromancer"), new Stats(300, 300, 300), 5,
                 true, i3, new HashSet<>(Arrays.asList(undeadTag, eliteTag)));
         sampleCharacter = new Character[] {c};
         sampleMob = new Mob[] {m1, m2, m3};
-        sampleItem = new Item[] {sword, bow, staff};
+        sampleItem = new Item[] {sword, bow, staff, spellBook};
     }
 
     /** Initialize Reroll with sample data */
